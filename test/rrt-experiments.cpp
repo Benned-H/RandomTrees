c87b768fce06ecbd6d61f5192c4985d13a6e1d8d
Benned-H <benned1@gmail.com>
2023-02-11 15:53:40 -0500

    Clean up language

--- test/rrt-experiments.cpp
+++ test/rrt-experiments.cpp
@@ -24,7 +24,7 @@ TEST_CASE( "RRT rapidly explores the 2D plane", "[RRT][experiment]" )
  * @brief RRT-found paths differ from optimal paths by a factor of 1.3 to 2.0.
  * 
  * @details Experimental details from LaValle (1998):
- *  - This finding occurred in 2D, convex spaces
+ *  - This finding applies to 2D, convex spaces
  */
 TEST_CASE( "RRT paths are longer than optimal paths", "[RRT][experiment]" )
 {
@@ -73,7 +73,7 @@ TEST_CASE( "RRT plans for an unstoppable, right-turn-only car", "[RRT][experimen
  * 
  * @details Experimental details from LaValle (1998):
  *  - This result is illustrated in the bottom left of the last figure
- *  - The figure shows rectangular, triangular, and hexgonal static obstacles
+ *  - The figure shows rectangular, triangular, and hexagonal static obstacles
  *  - The car can only move forward and turn left in three different increments
  *  - The car cannot move straight
  */
@@ -87,7 +87,7 @@ TEST_CASE( "RRT avoids obstacles with an uncontrollable car", "[RRT][experiment]
  * 
  * @details Experimental details from LaValle (1998):
  *  - This result is illustrated in Figure 1, and the bottom right of the last figure
- *  - The state space for the RRT is 5D, based on the caption for Figure 1
+ *  - Per the caption on Figure 1, this RRT has a 5D state space
  *  - The latter figure (bottom right) shows overlapping rectangular obstacles
  */
 TEST_CASE( "RRT avoids obstacles with a kinodynamic car", "[RRT][experiment]" )
